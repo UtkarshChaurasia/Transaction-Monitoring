267aa0bef655cf372c64c4cf5a80702ca70c117e
UtkarshChaurasia <msdianuc07@gmail.com>
2020-10-11 00:22:30 +0530

    setup.sh file added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"msdianuc07@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
